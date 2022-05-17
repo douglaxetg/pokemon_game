f3da78b576830bc4f9ed24d761176b532075ef1c
douglaxetg <douglastovar.g@gmail.com>
2022-05-17 10:01:42 -0400

    importing scripts for get pokemons

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -9,6 +9,10 @@
 <script>
 import PokemonOptions from "../components/PokemonOptions";
 import PokemonPicture from "../components/PokemonPicture";
+import getPokemonOptions from "../helpers/getPokemonOptions";
+
+console.log(getPokemonOptions());
+
 export default {
   components: { PokemonPicture, PokemonOptions },
 };
