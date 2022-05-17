16ea50ea7f469fca7be791663562a4afca2f4b3d
douglaxetg <douglastovar.g@gmail.com>
2022-05-17 00:49:48 -0400

    adding components

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Who is this Pokemon?</h1>
+  <!-- TODO: img -->
+  <PokemonPicture />
+  <PokemonOptions />
+  <!-- TODO: options -->
+</template>
+
+<script>
+import PokemonOptions from "../components/PokemonOptions";
+import PokemonPicture from "../components/PokemonPicture";
+export default {
+  components: { PokemonPicture, PokemonOptions },
+};
+</script>
+
+<style></style>
