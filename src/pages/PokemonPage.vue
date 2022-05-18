09862913c0b762566fbdf6d4815a2a4fbff9ae30
douglaxetg <douglastovar.g@gmail.com>
2022-05-18 01:50:00 -0400

    adding message and newgame button

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,11 +1,16 @@
 <template>
   <h1 v-if="!pokemon">Wait a moment please...</h1>
-  <div v-if="pokemon">
+
+  <div v-else>
     <h1>Who is this Pokemon?</h1>
     <!-- TODO: img -->
     <PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon" />
-    <PokemonOptions :pokemons="pokemonArr" @selection="checkAnswer($event)" />
+    <PokemonOptions :pokemons="pokemonArr" @selection="checkAnswer" />
     <!-- TODO: options -->
+    <template v-if="showAnswer">
+      <button @click="newGame">New Game</button>
+      <h2>{{ message }}</h2>
+    </template>
   </div>
 </template>
 
@@ -23,6 +28,8 @@ export default {
       pokemonArr: [],
       pokemon: null,
       showPokemon: false,
+      showAnswer: false,
+      message: "",
     };
   },
   methods: {
@@ -34,7 +41,19 @@ export default {
     },
     checkAnswer(pokemonId) {
       this.showPokemon = true;
-      console.log("check Answer", pokemonId);
+      this.showAnswer = true;
+      if (pokemonId === this.pokemon.id) {
+        this.message = `Correct, it's ${this.pokemon.name}`;
+      } else {
+        this.message = `Oops, it was ${this.pokemon.name}`;
+      }
+    },
+    newGame() {
+      (this.pokemonArr = []),
+        (this.showPokemon = false),
+        (this.showAnswer = false),
+        this.mixPokemonArray();
+      this.pokemon = null;
     },
   },
   mounted() {
